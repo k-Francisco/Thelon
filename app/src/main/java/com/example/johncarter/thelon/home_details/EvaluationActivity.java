eac63910e0f91acb62e839e36f92a6e77e7b785b
Charles <Charles>
2017-03-17 22:19:45 +0800

    with points

--- app/src/main/java/com/example/johncarter/thelon/home_details/EvaluationActivity.java
+++ app/src/main/java/com/example/johncarter/thelon/home_details/EvaluationActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.johncarter.thelon.R;
@@ -15,11 +16,24 @@ import com.example.johncarter.thelon.main_screens.LandingPage;
  */
 
 public class EvaluationActivity extends AppCompatActivity {
+
+    RatingBar energyRating,passionRating,flexibilityRating,creativityRating;
+    RatingBar teamPlayerRating,friendlinessRating,reliabilityRating,commitmentRating;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.volunteer_evaluation_layout);
+
+        energyRating = (RatingBar) findViewById(R.id.rb1);
+        passionRating = (RatingBar) findViewById(R.id.rb2);
+        flexibilityRating = (RatingBar) findViewById(R.id.rb3);
+        creativityRating = (RatingBar) findViewById(R.id.rb4);
+        teamPlayerRating = (RatingBar) findViewById(R.id.rb5);
+        friendlinessRating = (RatingBar) findViewById(R.id.rb6);
+        reliabilityRating = (RatingBar) findViewById(R.id.rb7);
+        commitmentRating = (RatingBar) findViewById(R.id.rb8);
 
         TextView finishBtn = (TextView) findViewById(R.id.finishBtn);
         finishBtn.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +43,14 @@ public class EvaluationActivity extends AppCompatActivity {
                 startActivity(new Intent(EvaluationActivity.this, LandingPage.class));
             }
         });
+
+        float points = (energyRating.getRating() * 10) +
+                    (passionRating.getRating() * 10) +
+                    (flexibilityRating.getRating() * 10) +
+                    (creativityRating.getRating() * 10) +
+                    (teamPlayerRating.getRating() * 10) +
+                    (friendlinessRating.getRating() * 10) +
+                    (reliabilityRating.getRating() * 10) +
+                    (commitmentRating.getRating() * 10);
     }
 }
---
+++ app/src/main/java/com/example/johncarter/thelon/home_details/activity_volunteer_success.java
@@ -0,0 +1,15 @@
+package com.example.johncarter.thelon.home_details;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.johncarter.thelon.R;
+
+public class activity_volunteer_success extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteer_success);
+    }
+}
